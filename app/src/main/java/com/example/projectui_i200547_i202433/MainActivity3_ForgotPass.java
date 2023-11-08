b8da014ef981466c3ed767f46b8eeb0f7c124483
KashanAltaf <kashanaltaf137@gmail.com>
2023-11-08 18:40:13 +0500

    Project UI

---
+++ app/src/main/java/com/example/projectui_i200547_i202433/MainActivity3_ForgotPass.java
@@ -0,0 +1,14 @@
+package com.example.projectui_i200547_i202433;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity3_ForgotPass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_3_forgot_pass);
+    }
+}
