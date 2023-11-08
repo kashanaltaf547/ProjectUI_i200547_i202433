603a61cb056f809b6eb2153c44649afa67b484a9
KashanAltaf <kashanaltaf137@gmail.com>
2023-11-08 18:16:51 +0500

    Project UI

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ProjectUI_i200547_i202433"
+include(":app")
