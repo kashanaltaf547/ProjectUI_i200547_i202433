a9d3afa8e65f78e2fb55a77b0507f2c170de9015
KashanAltaf <kashanaltaf137@gmail.com>
2023-11-08 20:12:06 +0500

    Project UI

---
+++ app/src/main/java/com/example/projectui_i200547_i202433/MainActivity4_SignUp.java
@@ -0,0 +1,14 @@
+package com.example.projectui_i200547_i202433;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity4_SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_4_sign_up);
+    }
+}
